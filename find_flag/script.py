fe59a9fe2e42d4b94535e60b66d7ad79de228ca9
joppe2001 <joppe.montezinos@gmail.com>
2023-11-23 16:07:57 +0100

    Predict the landmass for a new flag
    Create a new flag dataset with specific attributes and use a decision
    tree model to predict the landmass. The new flag has red, blue, and
    white colors, 13 stripes, and 50 stars. Other attributes are absent. The
    decision tree is trained on existing flag data with accurate predictors.
    The prediction result is mapped to a landmass using a pre-defined
    dictionary. This allows us to determine which continent the new flag
    likely belongs to.
    
    Fixes #123

--- find_flag/script.py
+++ find_flag/script.py
@@ -15,6 +15,14 @@ df= pd.read_csv("flag.data", names = cols)
 #variable names to use as predictors
 var = [ 'red', 'green', 'blue','gold', 'white', 'black', 'orange', 'mainhue', 'bars','stripes', 'circles','crosses', 'saltires','quarters','sunstars','triangle','animate']
 
+cont_1 = 'N.America'
+cont_2 = 'S.America'
+cont_3 = 'Europe'
+cont_4 = 'Africa'
+cont_5 = 'Asia'
+cont_6 = 'Oceania'
+
+continent_dict = {1:cont_1, 2:cont_2, 3:cont_3, 4:cont_4, 5:cont_5, 6:cont_6}
 #Print number of countries by landmass, or continent
 print(df.groupby('landmass')['name'].count())
 
@@ -34,7 +42,7 @@ print(df[var].dtypes)
 data = pd.get_dummies(df[var])
 
 #Split data into a train and test set
-x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size = 0.3, random_state = 1)
+x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size = 0.2, random_state = 1)
 
 #Fit a decision tree for max_depth values 1-20; save the accuracy score in acc_depth
 depths = range(1, 21)
@@ -75,26 +83,37 @@ for ccpa in ccp_alpha:
     acc_ccp.append(dtree.score(x_test, y_test))
 
 
-#Plot the accuracy vs ccp_alpha
-sns.set_style("whitegrid")
-plt.plot(ccp_alpha, acc_ccp)
-plt.xlabel("ccp_alpha")
-plt.ylabel("Accuracy")
-plt.show()
+new_flag_attributes = {
+    'red': [1],  # Red is present
+    'green': [0],  # Green is not present
+    'blue': [1],  # Blue is present
+    'gold': [0],  # Gold is not present
+    'white': [1],  # White is present
+    'black': [0],  # Black is not present
+    'orange': [0],  # Orange is not present
+    'mainhue': ['blue'],  # Main color can be considered blue
+    'bars': [0],  # No bars
+    'stripes': [5],  # 13 stripes
+    'circles': [0],  # No circles
+    'crosses': [0],  # No crosses
+    'saltires': [0],  # No saltires
+    'quarters': [0],  # No quarters
+    'sunstars': [0],  # 50 stars
+    'triangle': [0],  # No triangles
+    'animate': [0]  # No animate features
+}
 
-# Find the largest accuracy and the ccp value this occurs
-max_acc = max(acc_ccp)
-ccp_max = ccp_alpha[acc_ccp.index(max_acc)]
+# Convert to DataFrame
+new_flag_df = pd.DataFrame(new_flag_attributes)
 
-# Find the depth that gave the maximum accuracy
-max_depth = acc_depth.index(max(acc_depth)) + 1
+# Convert categorical variables to dummy variables
+new_flag_df = pd.get_dummies(new_flag_df)
 
-# Fit a decision tree model with the values for max_depth and ccp_alpha found above
-dtree.set_params(max_depth=max_depth, ccp_alpha=ccp_max)
-dtree.fit(x_train, y_train)
+# Make sure the new data has the same features as the training data, fill missing with 0s
+new_flag_df = new_flag_df.reindex(columns = x_train.columns, fill_value=0)
 
-# Plot the final decision tree with larger font size
-plt.figure(figsize=(15,10))  # Increase the size of the figure
-tree.plot_tree(dtree, fontsize=10)  # Increase the font size
-plt.show()
+# Predict the landmass
+predicted_landmass = dtree.predict(new_flag_df)
 
+# Print the predicted landmass using the dictionary created earlier
+print('Predicted landmass: ', continent_dict[predicted_landmass[0]])
